85ec535e464c80279c95448b80da867cf7d14919
rustynbustedup <2rustynbustedup@gmail.com>
2025-03-25 09:55:22 -0400

    IDK Whas up wit dis

---
+++ flashcards.py
@@ -0,0 +1,12 @@
+#SToopId Flashcard maker
+#if u do flashcard - Do it on paper
+
+
+
+import  json
+
+mode = input ("What mode do you want to enter? Teacher or Student?")
+
+class Teacher:
+    @staticmethod #use a variable outside smthn else
+
