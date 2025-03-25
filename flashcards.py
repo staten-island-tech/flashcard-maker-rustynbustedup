896b20ad9ed0fb6260d4c5b2783467cd7b6b38c1
rustynbustedup <2rustynbustedup@gmail.com>
2025-03-25 10:06:53 -0400

    I HATE JSON

--- flashcards.py
+++ flashcards.py
@@ -9,4 +9,10 @@ mode = input ("What mode do you want to enter? Teacher or Student?")
 
 class Teacher:
     @staticmethod #use a variable outside smthn else
+    def flashcard_maker():
+        try:
+            with open ("flashcards.json", "r") as file:
+                flashcards = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            
 
