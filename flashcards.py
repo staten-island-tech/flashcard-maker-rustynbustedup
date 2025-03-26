bad7026eb1059db25734bed36ffab2c9cf5a2a3a
rustynbustedup <2rustynbustedup@gmail.com>
2025-03-26 10:07:44 -0400

    finito it

--- flashcards.py
+++ flashcards.py
@@ -34,13 +34,41 @@ class Teacher:
 class Student:
     @staticmethod 
     def study_flashcards():
+        streak = 0
+        points = 0
         try:
             with open("flashcards.json", "r" ) as file:
                 flashcards = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            flashcards = {}
 
+
+        if not flashcards:
+            print ("Your lazy teach made no flashcards for u")
+        else:
+            for question, answer in flashcards.items():
+                print (f"Question: {question}")
+                user_answer = input ("What is your answer?")
+                if user_answer.lower() == answer.lower():
+                    print ("Correct")
+                    points += 1
+                    streak += 1
+                    print(points)
+                else:
+                    print (f"Wrong, the answer is {answer}")
+                    streak = 0 
+                if streak >= 3:
+                    print ("You are on fire")
+                    points = points + 2 
     
+
+
 if mode.lower() == "teacher":
     Teacher.flashcard_maker()
+elif mode.lower() == "student":
+    Student.study_flashcards()
+else:
+    print("Unknown input")
 
     
 
